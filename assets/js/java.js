28046b2aba60eaf6f96a779aefec30b97afec241
RafaelGaJO <167353727+RafaelGaJO@users.noreply.github.com>
2024-09-04 15:58:43 -0300

    Add files via upload

---
+++ assets/js/java.js
@@ -0,0 +1,37 @@
+
+function modoescuro (estado){
+
+    if (estado==true) {
+    
+    document.getElementById("corpy").style.background = '#1F1E38'
+    document.getElementById("cabeça").style.background='#252440'
+    document.getElementById("card").style.background='#252440'
+    document.getElementById("mg").style.background=  '#454570'
+    document.getElementById("usu").style.background=  '#454570'
+    document.getElementById("ss").style.background=  '#454570'
+    document.getElementById("sp").style.background=  '#454570'
+    document.getElementById("ssl").style.background=  '#454570'
+    document.getElementById("so").style.background=  '#454570'
+    document.getElementById("oo").style.color=  '#f0f0f5'
+    document.getElementById("op").style.color=  '#f0f0f5'
+    document.getElementById("ol").style.color=  '#f0f0f5'
+     }
+
+    else{
+    
+    document.getElementById("corpy").style.background = '#f0f0f5'
+    
+    document.getElementById("cabeça").style.background=  '#ffffff'
+    document.getElementById("card").style.background=  '#ffffff'
+    document.getElementById("mg").style.background=  '#ffe3e0'
+    document.getElementById("usu").style.background=  '#ffe3e0'
+    document.getElementById("ss").style.background=  '#ffe3e0'
+    document.getElementById("sp").style.background=  '#ffe3e0'
+    document.getElementById("ssl").style.background=  '#ffe3e0'
+    document.getElementById("so").style.background=  '#ffe3e0'
+    document.getElementById("oo").style.color=  '#000 '
+    document.getElementById("op").style.color=  '#000'
+    document.getElementById("ol").style.color=  '#000'
+    
+}
+    }   
